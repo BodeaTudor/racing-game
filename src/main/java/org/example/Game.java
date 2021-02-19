9847226c1512fff99b313db7119bb45ff59126ff
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-02-19 21:08:56 +0200

    *studied how to get input from the user

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -2,18 +2,25 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Game {
 
     private Track[] tracks = new Track[3];
     private List<Vehicle> players = new ArrayList<>();
+    // The difference between ArrayList<> and LinkedList is that ArrayList doesn't guarantee that the order of the elements
+    // in the array is kept.
+    // !!!SAME GOES FOR HashMap<> and SortedMap<>!!!
+    // List and Set are collections the difference is that the elements from Set are unique.
+    // Map<key, value> Example: Map<String, Vehicle> map = new HashMap<>() or new SortedMap<>().
+    // Map is a collection of key-value pair.
 
     public void start() {
         initializeTracks();
         displayTracks();
 
-        int playersCount = 2;
+        int playersCount = getPlayerCountFromUser();
 
         for (int i = 0; i < playersCount; i++) {
             addPlayers();
@@ -67,12 +74,18 @@ public class Game {
 
     private void addPlayers() {
         Vehicle vehicle = new Vehicle();
-        vehicle.setName("Mercedes");
+        vehicle.setName(getVehicleNameFromUser());
         vehicle.setFuelLevel(60);
         vehicle.setMileage(ThreadLocalRandom.current().nextDouble(4.5, 20));
         vehicle.setMaxSpeed(200);
 
         players.add(vehicle);
+    }
+
+    private String getVehicleNameFromUser() {
+        System.out.println("Please enter vehicle name for player: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 
     private void displayPlayers() {
@@ -83,5 +96,11 @@ public class Game {
                 System.out.println((i + 1) + ". " + players.get(i).getName() + " - mileage: " + players.get(i).getMileage());
             }
         }
+    }
+
+    private int getPlayerCountFromUser() {
+        System.out.println("Please specify the number of players (use numbers only): ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
     }
 }
